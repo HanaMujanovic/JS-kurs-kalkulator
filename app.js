6514b32070c8ea524ae4bf2a300cff81f9e2f392
HanaMujanovic <166912620+HanaMujanovic@users.noreply.github.com>
2024-04-14 13:06:30 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("radi!")
+function sabiranje (broj1, broj2){
+    let rezultatSabiranja = broj1+broj2;
+}
+
+function oduzimanje (broj1, broj2){
+    let rezultatOduzimanja = broj1+broj2;
+}
+
+function dijeljenje (broj1, broj2){
+    let rezultatDijeljenja = broj1+broj2;
+}
+
+function mnozenje (broj1, broj2){
+    let rezultatMnozenja = broj1+broj2;
+}
