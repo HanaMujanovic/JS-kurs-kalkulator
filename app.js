f914e0c6b6d7fa221d3b9a7a3851fbf934656e71
HanaMujanovic <166912620+HanaMujanovic@users.noreply.github.com>
2024-04-14 14:01:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
+let broj1=10;
+let broj2=5;
+
 function sabiranje (broj1, broj2){
     let rezultatSabiranja = broj1+broj2;
+    console.log(rezultatSabiranja);
 }
 
 function oduzimanje (broj1, broj2){
     let rezultatOduzimanja = broj1+broj2;
+    console.log(rezultatOduzimanja);
 }
 
 function dijeljenje (broj1, broj2){
     let rezultatDijeljenja = broj1+broj2;
+     console.log( rezultatDijeljenja);
 }
 
 function mnozenje (broj1, broj2){
     let rezultatMnozenja = broj1+broj2;
+     console.log(rezultatMnozenja);
 }
+
+let zbir=sabiranje(broj1,broj2);
+let razlika=oduzimanje(broj1,broj2);
+let kolicnik=dijeljenje(broj1,broj2);
+let proizvod=mnozenje(broj1,broj2);
+
+console.log(zbir);
+console.log(razlika);
+console.log(kolicnik);
+console.log(proizvod);
+
